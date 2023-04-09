835470eeb5e3498ef02285b6c6eb1295cb818f93
kovs705 <smartkovsik@gmail.com>
2023-04-09 11:09:06 +0300

    Initial commit for SWIFT MARATHON

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
+        // new AVAudio player
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        //print(sender.currentTitle)
+        playSound(soundName: sender.currentTitle!)
+        // ! means it will always have a title
+        
+        sender.alpha = 0.5
+        // lower the opacity by half
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            // brings back the last opacity to the button:
+            sender.alpha = 1.0
+        }
+    }
+    
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        // get audio
+        player = try! AVAudioPlayer(contentsOf: url!)
+        // put file into player
+        player.play()
+        // play the sound
+    }
 }
 
